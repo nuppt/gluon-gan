dc2575b0da0a2bae516e1cd55947c9430cfcbfcf
richyang <nuppt@mail.ustc.edu.cn>
2019-06-09 23:27:34 +0800

    cyclegan: add README to tools of data downloading

---
+++ cyclegan/tools/README.md
@@ -0,0 +1,17 @@
+# Download all datasets
+
+```bash
+bash download_all.sh <DATASET_TARGET_DIR>
+```
+* example
+```bash
+bash download_all.sh /home/funny_guy/Datasets/
+```
+
+# Download one specific dataset
+```bash
+bash download_dataset.sh <DATASET_TARGET_DIR> <DATASET_NAME>
+```
+```example
+bash download_all.sh /home/funny_guy/Datasets/ ae_photos
+```
--- cyclegan/tools/download_all.sh
+++ cyclegan/tools/download_all.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 
+DATASET_DIR=$1
+
 declare -a FILE_LIST=("ae_photos" "apple2orange" "summer2winter_yosemite" "horse2zebra" "monet2photo" "cezanne2photo" "ukiyoe2photo" "vangogh2photo" "maps" "cityscapes" "facades" "iphone2dslr_flower")
 
 ## now loop through the above array
 for i in "${FILE_LIST[@]}"
 do
-   bash ./download_dataset.sh /home/rich/Desktop/Datasets $i &
+   bash ./download_dataset.sh $1 $i &
    # or do whatever with individual element of the array
 done
 
