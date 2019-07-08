2c3178f93ea8b3a8b1fb0cb90bf3100255ec5dee
nuppt <nuppt@mail.ustc.edu.cn>
2019-07-09 00:57:05 +0800

    cgan: debug G

--- conditional-gan/main.py
+++ conditional-gan/main.py
@@ -6,18 +6,19 @@ from mxnet.gluon.data import DataLoader
 from dataset import MNISTDataset
 from transform import transform
 from model import *
+import numpy as np
+from utils import *
+from mxnet import init, nd
 
 
 @ex.config
 def config():
     opt = args
-    print(opt)
+    pprint(opt)
 
 
 def dot_opt(opt):
     opt = DotMap(opt)
-    print(f"Experiment config opt:")
-    pprint(opt)
     return opt
 
 
@@ -25,16 +26,19 @@ def dot_opt(opt):
 def main(opt):
     opt = dot_opt(opt)
 
-    # datasets
-    mnist_train_dataset = MNISTDataset(train=True, transform=transform)
-    assert mnist_train_dataset
-
-    dataloader = DataLoader(mnist_train_dataset, batch_size=opt.batch_size,
-                            shuffle=True, last_batch='discard',
-                            pin_memory=True, num_workers=opt.num_workers)
-    print("Data ready...")
+    # # datasets
+    # mnist_train_dataset = MNISTDataset(train=True, transform=transform)
+    # assert mnist_train_dataset
+    #
+    # dataloader = DataLoader(mnist_train_dataset, batch_size=opt.batch_size,
+    #                         shuffle=True, last_batch='discard',
+    #                         pin_memory=True, num_workers=opt.num_workers)
+    # print("Data ready...")
 
     # Conditional GAN: G and D
     netG = ConditionalG(opt)
-
-    
+    ctx = try_gpu()
+    netG.initialize(init.Xavier(factor_type='in', magnitude=0.01), ctx=ctx)
+    init_z = nd.array(np.ones(shape=(opt.batch_size, opt.nz)), ctx=ctx)
+    init_label = nd.array(np.ones(shape=(opt.batch_size, opt.num_classes)), ctx=ctx)
+    netG(init_z, init_label)
--- conditional-gan/model.py
+++ conditional-gan/model.py
@@ -10,7 +10,11 @@ class ConditionalG(nn.Block):
         self._build_network()
 
     def forward(self, latent, label):
-        pass
+        latent = self.latent_in_blk(latent)
+        label = self.label_in_blk(label)
+        x = self.mix_in_blk(latent, label)
+        out = self.out_blk(x)
+        return out
 
     def _build_network(self):
         #####################
@@ -18,13 +22,42 @@ class ConditionalG(nn.Block):
         #####################
         self.latent_in_blk = nn.Sequential()
         with self.latent_in_blk.name_scope():
-            self.latent_in_blk.add([nn.Dense(units=self.opt.num_hidden, in_units=self.opt.num_z, activation='relu'),
-                                    nn.LeakyReLU(0.2),])
+            self.latent_in_blk.add(nn.Dense(units=self.opt.z_dim, in_units=self.opt.nz, activation='relu'))
+            self.latent_in_blk.add(nn.LeakyReLU(0))
 
         self.label_in_blk = nn.Sequential()
         with self.label_in_blk.name_scope():
-            self.label_in_blk.add([nn.Embedding(input_dim=self.opt.num_classes, output_dim=self.opt.embeding_size),
-                                   nn.Dense(units=self.opt.num_hidden)])
+            # self.label_in_blk.add(nn.Embedding(input_dim=self.opt.num_classes, output_dim=self.opt.embed_dim))
+            # self.label_in_blk.add(nn.Dense(units=self.opt.ngf))
+            self.label_in_blk.add(nn.Dense(units=self.opt.label_dim, in_units=self.opt.num_classes, activation='relu'))
+            self.label_in_blk.add(nn.LeakyReLU(0))
+
+        self.mix_in_blk = nn.Lambda(lambda x, y: nd.concatenate([x, y], axis=1))
+        self.out_blk = nn.Dense(units=self.opt.out_dim, activation='sigmoid')
+
+
+class ConditionalD(nn.Block):
+    """Discriminator in Conditional GAN."""
+    def __init__(self, opt):
+        super(ConditionalD, self).__init__()
+        self.opt = opt
+        self._build_network()
+
+    def forward(self, image, label):
+        img_tensor = nd.reshape(image, shape=(self.opt.batch_size, self.opt.image_size, self.opt.image_size, self.opt.nc))
+        x = self.mix_in_blk(img_tensor, self.label_in_blk(label))
+        x = self.backbone(x)
+        out = self.out_blk(x)
+        return out
+
+    def _build_network(self):
+        #####################
+        # input layers: label
+        #####################
+        self.label_in_blk = nn.Sequential()
+        with self.label_in_blk.name_scope():
+            self.label_in_blk.add(nn.Embedding(input_dim=self.opt.num_classes, output_dim=self.opt.embeding_size))
+            self.label_in_blk.add(nn.Dense(units=self.opt.ndf))
 
         self.mix_in_blk = nn.Lambda(lambda x, y: nd.concatenate(x, y))
 
@@ -33,10 +66,11 @@ class ConditionalG(nn.Block):
         #####################
         self.backbone = nn.Sequential()
         with self.backbone.name_scope():
-            self.backbone.add([nn.Conv2DTranspose(channels=128, kernel_size=(4,4), strides=(2,2), padding='same'),
-                               nn.LeakyReLU(0.2)])
-            self.backbone.add([nn.Conv2DTranspose(channels=128, kernel_size=(4,4), strides=(2,2), padding='same'),
-                               nn.LeakyReLU(0.2)])
+            self.backbone.add(nn.Conv2DTranspose(channels=128, kernel_size=(4, 4), strides=(2, 2)))
+            self.backbone.add(nn.LeakyReLU(0.2))
+            self.backbone.add(nn.Conv2DTranspose(channels=128, kernel_size=(4, 4), strides=(2, 2)))
+            self.backbone.add(nn.LeakyReLU(0.2))
+            self.backbone.add(nn.Flatten())
+            self.backbone.add(nn.Dropout(0.5))
 
-        self.out_blk = nn.Conv2D(channels=self.opt.nc, kernel_size=(7,7), activation='tanh', padding='same')
-
+        self.out_blk = nn.Dense(units=1, activation='sigmoid')
